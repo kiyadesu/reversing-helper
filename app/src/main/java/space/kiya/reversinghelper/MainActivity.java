2d853f75a08ca9dccd2d2e7b4faa2896b497a38b
kiya-z <2267161320@qq.com>
2016-01-21 18:27:17 +0800

    update

--- app/src/main/java/space/kiya/reversinghelper/MainActivity.java
+++ app/src/main/java/space/kiya/reversinghelper/MainActivity.java
@@ -3,6 +3,11 @@ package space.kiya.reversinghelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ *
+ * Created by kiya on 16-1-21.
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
--- app/src/main/java/space/kiya/reversinghelper/SmaliInjection/KiyaLog.java
+++ app/src/main/java/space/kiya/reversinghelper/SmaliInjection/KiyaLog.java
@@ -2,6 +2,19 @@ package space.kiya.reversinghelper.SmaliInjection;
 
 import android.util.Log;
 
+import java.util.Arrays;
+
+/**
+ *
+ * Created by kiya on 16-1-21.
+ */
+
+/*
+* 在反编译的索马里根目录下创建目录 space/kiya/reversinghelper/SmaliInjection/
+* 放入对应 smali 文件,即可调用
+* 调用语句：invoke-static {v1}, Lspace/kiya/reversinghelper/SmaliInjection/KiyaLog;->Log(Ljava/lang/Object;)V
+*/
+
 public class KiyaLog{
     public static void Log(String tag, String msg){
         Log.d(tag, msg);
@@ -12,6 +25,6 @@ public class KiyaLog{
     }
 
     public static void Log(Object[] objs){
-        Log("kiya",Arrays.toString(objs));
+        Log("kiya", Arrays.toString(objs));
     }
 }
